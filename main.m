2c3980ae8b60738fb3740a043d8faf031e35e823
Dieter Komendera <dieter@komendera.com>
2007-11-20 21:52:27 +0100

    add support for more outputtypes, make args optional

--- main.m
+++ main.m
@@ -3,7 +3,6 @@
 //  quickiee
 //
 //  Created by Dieter Komendera on 11/14/07.
-//  Copyright __MyCompanyName__ 2007. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
@@ -12,15 +11,48 @@
 
  int main(int argc, char *argv[])
  {
-	NSString *srcfile = [[[NSString alloc] initWithCString:argv[1] encoding: NSUTF8StringEncoding] stringByExpandingTildeInPath];
-    NSString *dstfile = [[[NSString alloc] initWithCString:argv[2] encoding: NSUTF8StringEncoding] stringByExpandingTildeInPath];
+    if(argc < 3) {
+        return -1;
+    }
+	NSString *srcfile = [[NSString stringWithUTF8String: argv[1]] stringByExpandingTildeInPath];
+    NSString *dstfile = [[NSString stringWithUTF8String: argv[2]] stringByExpandingTildeInPath];
+
+
+    NSSize dstsize;
+    if(argc >= 4) {
+        NSArray *sizes = [[NSString stringWithUTF8String: argv[3]] componentsSeparatedByString:@"x"];
+        int maxwidth   = [[sizes objectAtIndex: 0] intValue];
+        int maxheight  = [[sizes objectAtIndex: 1] intValue];        
+        dstsize = NSMakeSize(maxwidth, maxheight);
+    } else {
+        dstsize = NSMakeSize(200,200);
+    }
+    
+    NSString *format;
+    if(argc >= 5) {
+        format = [NSString stringWithUTF8String:argv[4]];
+    } else {
+        format = @"jpg";
+    }
+    
 	NSImage *image = [NSImage imageWithPreviewOfFileAtPath:srcfile
-		ofSize:NSMakeSize(300, 300)
+		ofSize:dstsize
 		asIcon:NO];
 	
     NSBitmapImageRep *rep = [[image representations] objectAtIndex: 0];
     
-    NSData *data = [rep representationUsingType: NSPNGFileType properties: nil];
+    NSData *data;
+    if([format caseInsensitiveCompare:@"jpg"] == 0) {
+        data =  [rep representationUsingType: NSJPEGFileType properties: nil];
+    } else if([format caseInsensitiveCompare:@"tiff"] == 0) {
+        data = [rep representationUsingType: NSTIFFFileType properties: nil];
+    } else if([format caseInsensitiveCompare:@"bmp"] == 0) {
+        data = [rep representationUsingType: NSBMPFileType properties: nil];
+    } else if([format caseInsensitiveCompare:@"gif"] == 0) {
+        data = [rep representationUsingType: NSGIFFileType properties: nil];
+    } else {
+        data = [rep representationUsingType: NSPNGFileType properties: nil];
+    }
     [data writeToFile: dstfile atomically: NO];
 
  	return 0;	
