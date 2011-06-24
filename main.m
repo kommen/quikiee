ec0294a07601117c9b5cea23aeeb2bf3fc174efe
Dieter Komendera <dieter@komendera.com>
2011-06-24 10:27:20 +0200

    Whitespace cleanup.

--- main.m
+++ main.m
@@ -10,59 +10,57 @@
 #import <QuickLook/QuickLook.h>
 #import "NSImage+QuickLook.h"
 
- int main(int argc, char *argv[])
- {
-   NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+int main(int argc, char *argv[]) {
+  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-    if(argc < 3) {
-        return -1;
-    }
-	NSString *srcfile = [[NSString stringWithUTF8String: argv[1]] stringByExpandingTildeInPath];
-    NSString *dstfile = [[NSString stringWithUTF8String: argv[2]] stringByExpandingTildeInPath];
+  if(argc < 3) {
+    return -1;
+  }
+  NSString *srcfile = [[NSString stringWithUTF8String: argv[1]] stringByExpandingTildeInPath];
+  NSString *dstfile = [[NSString stringWithUTF8String: argv[2]] stringByExpandingTildeInPath];
 
-    NSSize dstsize;
-    if(argc >= 4) {
-        NSArray *sizes = [[NSString stringWithUTF8String: argv[3]] componentsSeparatedByString:@"x"];
-        int maxwidth   = [[sizes objectAtIndex: 0] intValue];
-        int maxheight  = [[sizes objectAtIndex: 1] intValue];        
-        dstsize = NSMakeSize(maxwidth, maxheight);
-    } else {
-        dstsize = NSMakeSize(200,200);
-    }
-    
-    NSString *format;
-    if(argc >= 5) {
-        format = [NSString stringWithUTF8String:argv[4]];
-    } else {
-        format = @"png";
-    }
-    
-	NSImage *image = [NSImage imageWithPreviewOfFileAtPath:srcfile
-		ofSize:dstsize
-		asIcon:NO];
-   
-	
-    NSBitmapImageRep *rep = [[image representations] objectAtIndex: 0];
-    
-    NSData *data;
-    if([format caseInsensitiveCompare:@"png"] == 0) {
-        data =  [rep representationUsingType: NSJPEGFileType properties: nil];
-    } else if([format caseInsensitiveCompare:@"tiff"] == 0) {
-        data = [rep representationUsingType: NSTIFFFileType properties: nil];
-    } else if([format caseInsensitiveCompare:@"bmp"] == 0) {
-        data = [rep representationUsingType: NSBMPFileType properties: nil];
-    } else if([format caseInsensitiveCompare:@"gif"] == 0) {
-        data = [rep representationUsingType: NSGIFFileType properties: nil];
-    } else if([format caseInsensitiveCompare:@"jpg"] == 0){
-        data = [rep representationUsingType: NSPNGFileType properties: nil];
-    } else {
-        printf("unknown format: ");
-        printf([format UTF8String]);
-        printf("\n");
-        return -1;
-    }
-    [data writeToFile: dstfile atomically: NO];
+  NSSize dstsize;
+  if(argc >= 4) {
+    NSArray *sizes = [[NSString stringWithUTF8String: argv[3]] componentsSeparatedByString:@"x"];
+    int maxwidth   = [[sizes objectAtIndex: 0] intValue];
+    int maxheight  = [[sizes objectAtIndex: 1] intValue];        
+    dstsize = NSMakeSize(maxwidth, maxheight);
+  } else {
+    dstsize = NSMakeSize(200,200);
+  }
 
-    [pool release];
- 	return 0;	
- }
+  NSString *format;
+  if(argc >= 5) {
+    format = [NSString stringWithUTF8String:argv[4]];
+  } else {
+    format = @"png";
+  }
+
+  NSImage *image = [NSImage imageWithPreviewOfFileAtPath:srcfile
+                                                  ofSize:dstsize
+                                                  asIcon:NO];
+
+  NSBitmapImageRep *rep = [[image representations] objectAtIndex: 0];
+
+  NSData *data;
+  if([format caseInsensitiveCompare:@"png"] == 0) {
+      data =  [rep representationUsingType: NSJPEGFileType properties: nil];
+  } else if([format caseInsensitiveCompare:@"tiff"] == 0) {
+      data = [rep representationUsingType: NSTIFFFileType properties: nil];
+  } else if([format caseInsensitiveCompare:@"bmp"] == 0) {
+      data = [rep representationUsingType: NSBMPFileType properties: nil];
+  } else if([format caseInsensitiveCompare:@"gif"] == 0) {
+      data = [rep representationUsingType: NSGIFFileType properties: nil];
+  } else if([format caseInsensitiveCompare:@"jpg"] == 0){
+      data = [rep representationUsingType: NSPNGFileType properties: nil];
+  } else {
+    printf("unknown format: ");
+    printf([format UTF8String]);
+    printf("\n");
+    return -1;
+  }
+  [data writeToFile: dstfile atomically: NO];
+
+  [pool release];
+  return 0;	
+}
